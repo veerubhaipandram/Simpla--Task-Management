204ff1583fc3d797b7029ace7169bc5cb6fe9840
veerendra kumar pandram <120049307+veerubhaipandram@users.noreply.github.com>
2025-05-20 12:40:20 +0530

    Delete routes/profileRoutes.js

--- routes/profileRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getProfile } = require("../controllers/profileControllers");
-const { verifyAccessToken } = require("../middleware/authMiddleware");
-
-// Routes beginning with /api/profile
-router.get("/", verifyAccessToken, getProfile);
-
-module.exports = router;
